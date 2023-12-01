dfcd8d01e5c82cbd5127b3fa14b811f0089de05c
isaias lima <isaiaslima20182018@gmail.com>
2023-11-30 21:26:25 -0300

    deploy TExto no about corrigido

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,45 +1,66 @@
 <template>
-    <div>
-        <div class="container-sm bg-primary about">
-            <div class="row">
-                <div class="col-sm-12">
-                    <h2>Olá, sou Isaias de Lima sena!</h2>
-                    <h5>Desenvolvi esse sistema apartir do curso do professor Yuri Marcon - PokerVite
-                         com o objetivo de aprendizagem e aumentar meu portofólio.
-                         No projeto foram utilizadas as seguintes tecnologias: Vue3, Bootstrap 5, Pinia e Vue Router.
-                         <br />
-                         <hr>
-                        PokeApi: <a class="link-underline-light link-light" href="https://pokeapi.co/">https://pokeapi.co/</a>
-                        <br>
-                        VueJs: <a class="link-underline-light link-light" href="https://br.vuejs.org/">https://br.vuejs.org/</a>
-                        <br />
-                        Bootstrap: <a class="link-underline-light link-light" href="https://getbootstrap.com/">https://getbootstrap.com/</a>
-                        <br />
-                        Pinia: <a class="link-underline-light link-light" href="https://pinia.vuejs.org/">https://pinia.vuejs.org/</a>
-                    </h5>
-                </div>
-            </div>
+  <div>
+    <div class="container-sm bg-primary about">
+      <div class="row">
+        <div class="col-sm-12">
+          <h2>Olá, sou Isaias de Lima sena!</h2>
+          <h5>
+            Desenvolvi este
+            sistema a partir do curso do professor Yuri Marcon - Pokervite, com o
+            objetivo de aprendizado e para aumentar meu portfólio. No projeto,
+            foram utilizadas as seguintes tecnologias: Vue3, Bootstrap 5, Pinia
+            e Vue Router.
+            <br />
+            <hr />
+            PokeApi:
+            <a
+              class="link-underline-light link-light"
+              href="https://pokeapi.co/"
+              >https://pokeapi.co/</a
+            >
+            <br />
+            VueJs:
+            <a
+              class="link-underline-light link-light"
+              href="https://br.vuejs.org/"
+              >https://br.vuejs.org/</a
+            >
+            <br />
+            Bootstrap:
+            <a
+              class="link-underline-light link-light"
+              href="https://getbootstrap.com/"
+              >https://getbootstrap.com/</a
+            >
+            <br />
+            Pinia:
+            <a
+              class="link-underline-light link-light"
+              href="https://pinia.vuejs.org/"
+              >https://pinia.vuejs.org/</a
+            >
+          </h5>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
-
 </script>
 
 <style scoped>
-.about{
-    width: 90%;
-    height: 65vh;
-    border-radius: 5px;
-    margin-top: 40px;
-    padding: 20px;
-    color: antiquewhite;
-    overflow-y: auto;
+.about {
+  width: 90%;
+  height: 65vh;
+  border-radius: 5px;
+  margin-top: 40px;
+  padding: 20px;
+  color: antiquewhite;
+  overflow-y: auto;
 }
-a{
-    text-decoration: none;
-    text-decoration-color: aquamarine;
+a {
+  text-decoration: none;
+  text-decoration-color: aquamarine;
 }
-
 </style>
